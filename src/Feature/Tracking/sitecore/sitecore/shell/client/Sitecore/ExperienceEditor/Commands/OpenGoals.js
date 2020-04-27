95a49ac77a2c8e1e6366ee64fe246245609d8a4e
Corey Smith <coreyas@gmail.com>
2020-04-27 00:08:15 -0400

    Render improved tracking field controls in Experience Editor

---
+++ src/Feature/Tracking/sitecore/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/OpenGoals.js
@@ -0,0 +1,16 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+  Sitecore.Commands.OpenGoals =
+  {
+    canExecute: function (context) {
+      if (context.currentContext.isFallback) {
+        return false;
+      }
+
+      return context.app.canExecute("ExperienceEditor.OpenTrackingField.CanOpen", context.currentContext);
+    },
+    execute: function (context) {
+      context.currentContext.value = "/sitecore/shell/default.aspx?xmlcontrol=TreeListGoals";
+      ExperienceEditor.PipelinesUtil.initAndExecutePipeline(context.app.OpenTrackingFieldPipeline, context);
+    }
+  };
+});
---
+++ src/Feature/Tracking/sitecore/sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/OpenTrackingField/OpenTrackingField.OpenDialog.js
@@ -0,0 +1,9 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+  return ExperienceEditor.PipelinesUtil.generateDialogCallProcessor({
+    url: function (context) { return context.currentContext.value; },
+    features: "dialogHeight: 600px;dialogWidth: 800px;",
+    onSuccess: function (context, trackingField) {
+      context.currentContext.value = ExperienceEditor.Web.encodeHtml(trackingField);
+    }
+  });
+});
